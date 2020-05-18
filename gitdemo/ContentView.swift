8c44f480c55854581d950b974cf236a37ba2c541
MariannaKononenko <Marianna.Kononenko@jetbrains.com>
2020-05-18 17:25:57 +0200

    Add a new project to Git

---
+++ gitdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitdemo
+//
+//  Created by Marianna Kononenko on 18.05.20.
+//  Copyright © 2020 JetBrains. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
