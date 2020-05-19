dbb2a207c395562be9882b5a92d2c1bc458d2748
MariannaKononenko <Marianna.Kononenko@jetbrains.com>
2020-05-19 12:46:54 +0200

    Add List
    (cherry picked from commit e9958ba09ad2b53f9532cc7d21a52bb0392e4f40)

--- gitdemo/ContentView.swift
+++ gitdemo/ContentView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-            Text("Hello, World!")
+        List {
+            HStack {
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            VStack {
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
         }
     }
 }
